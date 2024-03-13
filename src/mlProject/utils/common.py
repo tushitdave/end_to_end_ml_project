c5ec5cb002c5b117da5ad956d20d1b4534bacb33
Tushit <tushitdavergtu@gmail.com>
2024-03-13 11:27:36 +0530

    requirements added

--- src/mlProject/utils/common.py
+++ src/mlProject/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from src.mlProject import logger
+from mlProject import logger
 import json
 import joblib
 from ensure import ensure_annotations
