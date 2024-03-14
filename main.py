53f8502f680d21144595e0f33fd58366df634568
Tushit <tushitdavergtu@gmail.com>
2024-03-14 15:22:21 +0530

    Changes made in common and config yaml

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from src.mlProject import logger
+# import sys
+# sys.path.append("d:\\Data Science\\end_to_end_ml_project\\src\\mlProject")
 
 logger.info("Welcome to our custom logging")
+logger.info("This is my logging Page")
