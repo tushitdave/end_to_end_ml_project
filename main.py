5e97d7d03b17c8536d62de4527afee3bf2586051
Tushit <tushitdavergtu@gmail.com>
2024-03-25 10:35:15 +0530

    Data Ingestion added

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from src.mlProject import logger
+from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
 # import sys
 # sys.path.append("d:\\Data Science\\end_to_end_ml_project\\src\\mlProject")
 
-logger.info("Welcome to our custom logging")
-logger.info("This is my logging Page")
+# logger.info("Welcome to our custom logging")
+# logger.info("This is my logging Page")
--- src/mlProject/pipeline/stage_01_data_ingestion.py
+++ src/mlProject/pipeline/stage_01_data_ingestion.py
@@ -15,7 +15,15 @@ class DataIngestionTrainingPipeline:
         data_ingestion.download_file()
         data_ingestion.extract_zip_file()
 
-
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
