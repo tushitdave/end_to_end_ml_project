d6f84913f1dd76f2253c0d43e4ae50c9285dce53
Tushit <tushitdavergtu@gmail.com>
2024-03-27 17:22:25 +0530

    model evaluation and MLFLOW added

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+
+import os
+os.environ["MLFLOW_TRACKING_URI"] = "https://dagshub.com/tushitdavergtu/end_to_end_ml_project.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"] = "tushitdavergtu"
+os.environ["MLFLOW_TRACKING_PASSWORD"] = "dbcfc4f0cb78a803fb5eb48d80c689a7af4dbc26"
+
 from src.mlProject import logger
 from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from src.mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from src.mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+
+
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -49,6 +58,16 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
+STAGE_NAME = "Model Evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 # import sys
 # sys.path.append("d:\\Data Science\\end_to_end_ml_project\\src\\mlProject")
